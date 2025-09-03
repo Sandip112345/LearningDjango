c99626cfd45e0074b709f2f9528f3a77364cd9ec
Sandip112345 <sandip2211175@iimscollege.edu.np>
2025-09-03 18:42:42 +0545

    django first projects

---
+++ django-polls/django-polls/admin.py
@@ -0,0 +1,25 @@
+from django.contrib import admin
+from .models import Question, Choice
+# Register your models here.
+
+class ChoiceInline(admin.TabularInline):
+        model = Choice
+        extra = 3
+        
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    # fields = ["publication_date", "question_text"]   # --> this will make publication_date appear above question_text
+    fieldsets = [
+        (None, {"fields": ["question_text"]}),
+        ("Date Information", {"fields": ["publication_date"]}),
+    ]
+    inlines = [ChoiceInline]
+
+    list_display = ("question_text", "publication_date", "was_published_recently")
+    list_filter = ["publication_date"]
+    search_fields = ["question_text"]
+admin.site.register(Question, QuestionAdmin)
+# admin.site.register(Choice)  
+
+
---
+++ firstproject/genre/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    #/genre/
+    path('',views.index.as_view(),name='index'),
+
+    #/genre/1/
+    path('<int:pk>/',views.details.as_view(),name='details'),
+
+    # register/
+    path('register/', views.UserFormView.as_view(), name='userform'),
+    
+
+]
